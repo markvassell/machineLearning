bbfda8b8dff80a28e46effe214fc9a6afd21f94a
markvassell <mdvy96@mail.missorui.edu>
2018-04-08 23:04:56 -0500

    fix the dir structure and added correlation for bot and non bot files.

--- LogisticRegression/logistic_regression.py
+++ LogisticRegression/logistic_regression.py
@@ -14,63 +14,71 @@ import pandas as pd
 from sklearn.metrics import confusion_matrix
 
 
+def get_correlation(outfile, data):
+    # check how correlated the attributes.  Below zero is less than 50% correlation
+    plt.figure()
+    sb.heatmap(data.corr(), annot=True, cmap="YlGnBu")
+    plt.savefig(outfile, format='png')
+    # plt.show()
 
-if __name__ == '__main__':
-    for name in ['zscore','recoded','ranked','rescaled']:
+
+def combined_embeddings(files):
+    for name in files:
         embedding = name
-        filename = 'Data/'+embedding+'.csv'
+        filename = 'Data/combined_embeddings/'+embedding+'.csv'
         posts = pd.read_csv(filename)
         print(posts.head())
 
         y = posts.ix[:, 3].values
         posts_data = X = posts.ix[:, (0, 1, 2)]
-        posts_data_name  = ['wdr','dissim','leven']
         print(y)
 
-        # check how correlated the attributes.  Below zero is less than 50% correlation
-        sb.heatmap(posts_data.corr(),annot=True,cmap="YlGnBu")
-        plt.savefig('Results/'+embedding+'/attribute_correlations.PNG', format='png')
-        plt.show()
-
-        # spearmanr_coefficient, p_value = spearmanr(wdr, leven, )
-        # print('Spearmnar Rank Coorelation Coefficient %0.3f' %spearmanr_coefficient)
-
+        get_correlation('Results/'+embedding+'/combined/attribute_correlations.PNG', posts_data)
 
         print(posts.isnull().sum())
         # Checking if our target is ordinary or binary
         sb.countplot(x='is_bot', data=posts, palette='hls')
-        plt.savefig('Results/'+embedding+'/binary_target_check.PNG', format='png')
+        plt.savefig('Results/'+embedding+'/combined/binary_target_check.PNG', format='png')
         plt.show()
         print(posts.info())
 
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.1, random_state=47)
 
-        LogReg = LogisticRegression()
-        LogReg.fit(X_train, y_train)
+        logReg = LogisticRegression()
+        logReg.fit(X_train, y_train)
 
-        y_pred = LogReg.predict(X_test)
+        y_pred = logReg.predict(X_test)
 
         my_confusion_matrix = confusion_matrix(y_test, y_pred)
         print('confusion_matrix')
         print(my_confusion_matrix)
         sb.heatmap(my_confusion_matrix,annot=True,fmt="d",cmap="YlGnBu",xticklabels=['bot','non-bot'], yticklabels=['bot','non-bot'])
-        plt.savefig('Results/'+embedding+'/confusion_matrix.PNG', format='png')
+        plt.savefig('Results/'+embedding+'/combined/confusion_matrix.PNG', format='png')
         plt.show()
 
-        with open('Results/'+embedding+'/stats.txt', 'w+') as statsFile:
+        with open('Results/'+embedding+'/combined/stats.txt', 'w+') as statsFile:
             statsFile.write(metrics.classification_report(y_test, y_pred))
-        # x = scale(posts_data)
-        # # initialize logistic regression model
-    # LogReg = LogisticRegression()
-    # LogReg.fit(x, y)
-    # # The closer to 1 the better the fit
-    # print('LogReg Score:', LogReg.score(x, y))
-    #
-    # # get predicted values
-    # y_pred = LogReg.predict(x)
-    #
-    # print(metrics.classification_report(y, y_pred))
 
+
+def separated_embeddings(files):
+    for file in files:
+        bot = 'Data/separated_embeddings/' + file + '/bot.csv'
+        bot = pd.read_csv(bot)
+        bot_data = bot.ix[:, (0, 1, 2)]
+        get_correlation('Results/' + file + '/bot/attribute_correlations.PNG', bot_data)
+
+        nonbot = 'Data/separated_embeddings/' + file + '/nonbot.csv'
+        nonbot = pd.read_csv(nonbot)
+        nonbot_data = nonbot.ix[:, (0, 1, 2)]
+        get_correlation('Results/' + file + '/nonbot/attribute_correlations.PNG', nonbot_data)
+
+
+if __name__ == '__main__':
+    files = ['zscore', 'recoded', 'ranked', 'rescaled']
+
+    #combined_embeddings(files)
+
+    separated_embeddings(files)
 
 '''
 Logistic regression is a statistical method for analyzing a dataset in which there are one or more independent variables that determine an outcome.
